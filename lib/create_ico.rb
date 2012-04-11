0c4fc2f544d1045e171e7f4281d5e75a9d14a0ea
rogerdpack <rogerpack2005@gmail.com>
2012-04-11 12:13:38 -0600

    oh yeah

--- lib/create_ico.rb
+++ lib/create_ico.rb
@@ -6,12 +6,14 @@ module M
   include_package 'java.awt.font'; [TextLayout]
   include_package 'javax.swing'; [JFrame, JLabel]
   
-  size = 14
-  image = BufferedImage.new(size,size, BufferedImage::TYPE_INT_ARGB);
-  graphics=g= image.createGraphics()
+  SIZE = 64
+  ICON_DIMENSION=SIZE-5
+  def self.get_letters_as_icon letters
+  
+  image = BufferedImage.new(SIZE,SIZE, BufferedImage::TYPE_INT_ARGB);
+  graphics = g = image.createGraphics()
   g.setColor(Color::WHITE )
-  g.fillRect(0,0,size,size)
-   
+  g.fillRect(0,0,SIZE,SIZE)
 
 =begin needed?  
    for (int col = 0; col < ICON_DIMENSION; col++) {
@@ -21,29 +23,26 @@ module M
    }
 =end
   
-  graphics.setRenderingHint(RenderingHints::KEY_TEXT_ANTIALIASING,
+   graphics.setRenderingHint(RenderingHints::KEY_TEXT_ANTIALIASING,
       RenderingHints::VALUE_TEXT_ANTIALIAS_ON);
    graphics.setRenderingHint(RenderingHints::KEY_ANTIALIASING,
       RenderingHints::VALUE_ANTIALIAS_ON);
       
-   ICON_DIMENSION=size-5
-   letter = '9';
    graphics.setFont(Font.new("Arial", Font::BOLD, ICON_DIMENSION-5));
    frc = graphics.getFontRenderContext();
-   mLayout = TextLayout.new(letter, graphics.getFont(), frc);
-
-   x = (-0.5 + (ICON_DIMENSION - mLayout.getBounds().getWidth()) / 2);
-   y = ICON_DIMENSION - ((ICON_DIMENSION - mLayout.getBounds().getHeight()) / 2);
+   
+   mLayout = TextLayout.new(letters, graphics.getFont(), frc)
+   y = ICON_DIMENSION - ((ICON_DIMENSION - mLayout.getBounds().getHeight()) / 2)
+   x = (ICON_DIMENSION - mLayout.getBounds().width) / 2
    graphics.setColor(Color::black);
+   graphics.drawString(letters, x, y);
+   image
+  end
 
-   graphics.drawString(letter, x, y);
-   $image = image
-  
    class J < JFrame
      def initialize
        super
-       self.icon_image=$image
-       p 'set it'
+       self.icon_images=[M.get_letters_as_icon('09')]
      end
    end
     
